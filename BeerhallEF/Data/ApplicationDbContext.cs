e1a051c1d07be3cfbf5c7b9142d179e4f2e86463
Nick Heymans <nick.heymans@hotmail.com>
2020-08-12 07:42:50 +0200

    Add project files.

---
+++ BeerhallEF/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerhallEF.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring = @"Server=.;Database=Beerhall;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
---
+++ BeerhallEF/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BeerhallEF.Data;
+
+namespace BeerhallEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                Console.WriteLine("Database created");
+            }
+        }
+    }
+}
